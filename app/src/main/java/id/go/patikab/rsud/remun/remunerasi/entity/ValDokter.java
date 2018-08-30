968c1692fdc04ed98467e416b0add1b2001b507b
novaariyanto <novasampoerna@gmail.com>
2018-08-30 08:43:02 +0700

    master

---
+++ app/src/main/java/id/go/patikab/rsud/remun/remunerasi/entity/ValDokter.java
@@ -0,0 +1,14 @@
+package id.go.patikab.rsud.remun.remunerasi.entity;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ValDokter {
+    @SerializedName("status")
+    String status;
+    @SerializedName("message")
+    String message;
+    @SerializedName("data")
+    List<>
+}
