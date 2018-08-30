28062e2369464156e49f9bef6d4709f3692400fa
LENOVO <novasampoerna@gmail.com>
2018-08-30 07:14:22 +0700

    fix xml

---
+++ app/src/main/java/id/go/patikab/rsud/remun/remunerasi/entity/ValDokter.java
@@ -0,0 +1,26 @@
+package id.go.patikab.rsud.remun.remunerasi.entity;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ValDokter {
+    @SerializedName("status")
+    String status;
+    @SerializedName("message")
+    String message;
+    @SerializedName("data")
+    List<DataDokter>dokterList;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<DataDokter> getDokterList() {
+        return dokterList;
+    }
+}
